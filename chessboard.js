103b3d7789ba9f8fae5c5ed1e6f575dfc110b4c6
Alexander Jack <jackx022@umn.edu>
2020-02-25 17:08:10 -0600

    add function setPieces() to pull out the functionality of setting pieces on the board from creating the board itself

--- chessboard.js
+++ chessboard.js
@@ -17,68 +17,47 @@ class Stack{
     }
 
 }
+
+function setPieces(){
+        var a0 = document.getElementById('a0');
+        var a2 = document.getElementById('a1');
+        var piece = document.createElement('div');
+        piece.className = 'piece';
+        piece.id = 'boo';
+        piece.setAttribute("isSelected", false);
+        var piece2 = document.createElement('div');
+        piece2.className = 'piece';
+        piece2.id = 'booff';
+        piece2.setAttribute("isSelected", false);
+        a0.appendChild(piece2);
+        a2.appendChild(piece);
+}
+
+
 function createChessBoard(){    
     var board = document.createElement('table');
     for(var i = 0; i < 8; i++){
         var row = document.createElement('tr');
         for(var j = 0;  j < 8; j++){
-            // check parity of square
             var square = document.createElement('td');
-
-            //square.className = "square";
-            //row[i][j].td = square;
             square.id = calcSquare(i, j);
             square.setAttribute('isSelected', false);
             square.addEventListener('click', function(){
-                //console.log('here');
-                //console.log(square.getAttribute('position'))
                 square.setAttribute('isSelected', true);
             });
-            /*
-                for(var i = 0; i < pieces.length; i++){
-                    if(pieces[i].getAttribute('isSelected') == 'true'){
-                        target.appendChild(pieces[i]);
-                        pieces[i].setAttribute('isSelected', false);
-                        target.setAttribute('isSelected', false);
-            }}});
-            */
             if((j+i)%2 === 0){
-                
-                if(square.getAttribute('id') == 'a0'){
-                    var piece = document.createElement('div');
-                    piece.className = 'piece';
-                    piece.id = 'boo';
-                    piece.setAttribute("isSelected", false);
-                    /*piece.addEventListener("click", function(){
-                        piece.setAttribute("isSelected", true)
-                    });*/
-                    square.appendChild(piece);
-                }
-                if(square.getAttribute('id') == 'a2'){
-                    var piece = document.createElement('div');
-                    piece.className = 'piece';
-                    piece.id = 'booff';
-                    piece.setAttribute("isSelected", false);
-                    /*piece.addEventListener("click", function(){
-                        piece.setAttribute("isSelected", true)
-                    });*/
-                    square.appendChild(piece);
-                }
-                
                 square.className = "blacksquare";
             }else{
-                // this always sets last square in the chessboard to be set
-                // add listener to squares to select square for rudpiece to move to 
-
                 square.className = "whitesquare";    
             }
                 row.appendChild(square);
                 }
                 board.appendChild(row);
+        
             }
     document.body.appendChild(board);
-    
 }
+
 
 // calcSquare takes in row and column numbers and converts them
 // to the unique code of the square
@@ -87,29 +66,18 @@ function calcSquare(row, column){
     return String.fromCharCode(97 + column) + row;
 }
 
-    /*square.addEventListener("click", function(){
-    pieces = document.getElementsByClassName('rudpiece');
-    squares = document.getElementsByClassName('blacksquare');
-    //console.log(squares.length);
-    for(var j = 0; j < squares.length; j++){
-        console.log(squares[j].getAttribute('id').concat(": ").concat(squares[j].getAttribute('isSelected')))
-        if(squares[j].getAttribute('isSelected') == 'true'){
-            var target = squares[j];
-            console.log('here');
-        }                   
-    }*/
-
-
-//
-//
-//
-
 function selectPiece(piece){
     piece.setAttribute("isSelected", true);
 }
 
 createChessBoard();
+setPieces();
 stack = new Stack();
+
+// takes in no arguments
+// return: bool
+// function determine whether or not there is a piece that is selected
+
 
 function areNoPiecesSelected(){
     var pieces = document.getElementsByClassName('piece');
